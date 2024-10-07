5acd5de7d68b8562af02a792bef4d7f20c6808b1
phanhung2702 <hungphanphan1996@gmail.com>
2024-10-07 12:26:28 +0700

    Đẩy code lên

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        
+        body: Container(
+          child: Center(
+            child: Text('Hello World!'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
